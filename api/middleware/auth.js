8e251682aecaa3b5177c83d16edb2a68b4612b9b
Marvin Sequera <marvinsequera@yahoo.com>
2022-06-10 23:49:06 +0200

    Token verification middleware

---
+++ api/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+const jwtToken = process.env.JWT_TOKEN
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers['x-calories-counter-token']
+
+    if (!token) {
+        return res.status(403).send("A token is required for authentication")
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtToken)
+        console.log(decoded)
+    } catch {
+        return res.status(401).send("Invalid Token")
+    }
+
+    return next()
+}
+
+export { verifyToken }
