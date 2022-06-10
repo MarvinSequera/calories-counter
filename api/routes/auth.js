1dd496c2555fc25b0da6eca991bcf633dd7512ad
Marvin Sequera <marvinsequera@yahoo.com>
2022-06-10 23:36:50 +0200

    singup and singin completed

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
+import jwt  from 'jsonwebtoken'
 import { db } from '../app.js'
 const routerAuth = express.Router()
 const saltRounds = process.env.SALT
-
-const test = '$2b$10$tqhZajQg9Reda1QgVopoX.J8d8JYWFMjAaVfNlA4YcTjITTKub5i6'
+const jwtToken = process.env.JWT_TOKEN
 
 routerAuth.post('/singup', async (req, res) => {
     const { 
@@ -28,46 +28,89 @@ routerAuth.post('/singup', async (req, res) => {
 
     const userCollection = db.collection('users')
 
-    const userExist = await userCollection.findOne({ email })
+    try {
+        const userExist = await userCollection.findOne({ email })
 
-    if (userExist) {
-        res.status(400).send(`Email Exist`)
-        return
+        if (userExist) {
+            res.status(400).send(`Email Exist`)
+            return
+        }
+        
+        const salt = await bcrypt.genSalt(+saltRounds)
+        const hash = await bcrypt.hash(password, salt)
+        const newUser = await userCollection.insertOne({
+            name,
+            email,
+            password: hash,
+            height,
+            weight,
+            age,
+            waist,
+            fat,
+            active: true,
+            diating: false,
+        })
+
+        if (!newUser) {
+            res.status(500).send('Error Saving New User')
+            return
+        }
+        
+        const { insertedId } = newUser
+
+        const token = jwt.sign(
+            { insertedId },
+            jwtToken,
+            { expiresIn: '48h' }
+        )
+        
+        res.status(200).json({ 
+            Message: 'Success User Created',
+            userId : insertedId,
+            token
+        })
+    } catch {
+        res.status(500).json({
+            errorMessage: 'Upps Something went wrong'
+        })
     }
-
-    const salt = await bcrypt.genSalt(+saltRounds)
-    const hash = await bcrypt.hash(password, salt)
-    const newUser = await userCollection.insertOne({
-        name,
-        email,
-        password: hash,
-        height,
-        weight,
-        age,
-        waist,
-        fat,
-        active: true,
-        diating: false,
-    })
-
-    if (!newUser) {
-        res.status(500).send('Error Saving New User')
-        return
-    }
-
-    const { insertedId } = newUser
-
-    res.json({ 
-        Message: 'Success User Created',
-        Id: insertedId
-    })
 })
 
-// TODO: login
-    // bcrypt.compare(password, test, (err, res) => {
-    //     if (err) {
-    //         console.error(err);
-    //     }
+routerAuth.post('/signin', async (req, res) => {
+    const { body: { password, email } } = req
+    const userCollection = db.collection('users')
 
+    try {
+
+        const user = await userCollection.findOne({ email })
+        
+        if (!user) {
+        res.status(403).send('Password or Email not valid')
+        return
+        }
+        
+        const { password: passwordStore, _id: userId, active } = user
+        
+        const passwordMatch = await bcrypt.compare(password, passwordStore)
+        
+        if (!passwordMatch || !active) {
+            res.status(403).send('Password or Email not valid')
+            return
+        }
+        
+        const token = jwt.sign(
+            { userId },
+            jwtToken,
+            { expiresIn: '48h' }
+        )
+
+        
+        res.status(201).json({ userId, token })
+    } catch {
+        res.status(500).json({
+            errorMessage: 'Upps Something went wrong'
+        })
+    }
+})
 
 export { routerAuth }
